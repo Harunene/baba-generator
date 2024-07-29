671e2d893e69a17a8cb7178a2ba3cb52e3078fa1
harunene <taskinrits@gmail.com>
2024-07-30 05:30:59 +0900

    이미지 추가, 메타데이터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,32 @@
 import Main from '@/components/main';
+import { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string }
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: 'baba generator',
+    description: 'baba 이미지 생성기',
+    openGraph: {
+      type: "article",
+      title: 'baba generator',
+      description: 'baba 이미지 생성기',
+      images: '/public/og-image.png'
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: 'baba generator',
+      description: 'baba 이미지 생성기',
+      images: '/public/og-image.png'
+    }
+  }
+}
 
 export default function Home() {
   return (
